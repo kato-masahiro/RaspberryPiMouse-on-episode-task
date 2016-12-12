b51ed3e179010c0480179f68d1c5a956dec3d595
kato-masahiro <s1426030hj@s.chibakoudai.jp>
2016-12-12 22:35:53 +0900

    関数voting追加
    もうちょっとで動く？
    voting、motion_updateを確認する必要がある
    voting -> decision_makig

--- particle_filter_on_episode.py
+++ particle_filter_on_episode.py
@@ -69,9 +69,9 @@ if os.path.exists("./episode_set.txt"):
 
 print episode_set
 
-########################################
-
-########################################
+####################################
+#   センサの平均値を求める関数     #
+####################################
 def sensors_ave():
     global rf;global rs;global ls;global lf
     global sensors_val
@@ -151,13 +151,51 @@ def motion_update(particle):
 
     return particle
 
+######################################
+#   投票によって行動を決定する関数   #
+######################################
+def voting(particle):
+    vote = range(1000):#各パーティクルが自分の所属しているエピソードに対して持つ評価
+    for i in (vote):
+        vote[i] = 0.0
+    if T == 1:#まだどんなエピソードも経験していないのでランダムに行動させる
+        return random.choice("frl")
+    else:#各パーティクルが投票で決める
+        non_zero_reword = 0.0
+        for l in range(len(episode_set) - particle[i][0] - 1):
+            distance += 1
+            if episode_set[ particle[i][0] + distance ][5] != 0:
+                non_zero_reword = float(episode_set[particle[i][0] + distance][5])
+                break
+            print "particle:",i," position:",particle[i][0]," distance:",distance," non_zero_reword:",non_zero_reword
+            if non_zero_reword != 0:
+                vote[i] = non_zero_reword / distance
+            else:
+                vote[i] = 0.0
+            print "vote:",vote[i]
+
+    #voteに基づく行動決定。voteの合計がゼロやマイナスになる可能性がある点に注意
+    got = {"f":0.0,"r":0.0,"l":0.0}
+    for i in range(1000):
+        if int(vote[i]) != 0:
+            print episode_set[particle[i][0]][4]
+            got [episode_set[particle[i][0]] [4]] += vote[i]
+        print got
+        #グリーディならgotの中で最大の数字を持つもののキーをひとつ返す
+        #参考:http://cointoss.hatenablog.com/entry/2013/10/16/123129
+        if (random.randint(1,100) > epsiron):
+            return max(gotitems(),key = lambda x:x[1])[0]
+        else:
+            print "random_choice"
+            return random.choice("frl")
+
 ##################################################
 #    センサ値をsubscribeするコールバック関数     #
 ##################################################
 def sensors_callback(message):
     global rf;global rs;global ls;global lf
     global sensors_val
-    global counter
+    globdal counter
     global latest_sen
 
     counter += 1
